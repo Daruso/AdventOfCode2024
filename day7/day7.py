a69f7f51a9d80012be1818f17aa2b4ebefd3d4e9
Danny <danny.janssen@dimaros.nl>
2024-12-19 00:16:07 +0100

    Day 7 part one, small cleanup

--- day7/day7.py
+++ day7/day7.py
@@ -1,39 +1,26 @@
 import itertools
 
-
 f = open('day7\\input.txt')
 
-lineDict = {}
+operators = {'*','+'}
+runningTotal = 0
 
 for line in f:
     key, values = line[0:-1].split(':')
-    # print(values.split(' '))
-    lineDict[int(key)] = [int(val) for val in values[1:].split(' ')]
+    key = key
+    values = [val for val in values[1:].split(' ')]
 
-# print(lineDict)
-
-operators = {'*','+'}
-
-runningTotal = 0
-
-for key, values in lineDict.items():
     # create permutations
-    permutations = []
     for perm in itertools.product(operators, repeat=len(values) - 1):
-        permutation = values[:]
-        index = 0
-        # Insert operators at specified positions
-        for j, operator in enumerate(perm):
-            permutation.insert(index + j + 1, operator)
-            index +=1
-        permutations.append(permutation)
-    
-    for perm in permutations:
-        # print("Key: ", key, "Calculation:", eval(''.join(map(str,perm))))
-        if (eval(''.join(map(str,perm)))) == key:
-            print("Found permutation: ", perm)
-            runningTotal += key
+        total = values[0]  # save the left-most value as the start of our total
+        
+        # create operation and value tuples, then evaluate
+        for (op, val) in zip(perm, values[1:]):
+            total = str(eval(''.join((total, op, val))))
 
+        # compare the value of the equation to the key
+        if total == key:
+            runningTotal += int(key)
+            break
 
 print(runningTotal)
-
